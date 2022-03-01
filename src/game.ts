aba013632e82a1bcf75147bc926ddcaf7d73c355
yeonsik <happyit84@gmail.com>
2022-03-01 11:21:11 +0900

    add log

--- src/game.ts
+++ src/game.ts
@@ -69,21 +69,31 @@ entity4.addComponentOrReplace(transform5)
 
 executeTask(async () => {
   try{
-    const address = await getUserAccount()
+    const address = (await getUserAccount()).toUpperCase()    
     log("user address: " + address) // add DEBUG_SCENE_LOG to url parameter to see log on deployed scene
-    if (address !=  "0x2127aD06A0D77cb8496383D637457EF5A325A8cC" && address != "0x77C233AbA2A296951BA15BD1f8496148d5eBde81")
+    if (address != "0x2127aD06A0D77cb8496383D637457EF5A325A8cC".toUpperCase() && 
+        address != "0x77C233AbA2A296951BA15BD1f8496148d5eBde81".toUpperCase())
+    {
+      log("NOT Privileged User")
       return;
+    }
+      
 
     const provider = await getProvider()
+    log("after getProvide")
     const requestManager = new RequestManager(provider)
+    log("after requestManager")
     const factory = new ContractFactory(requestManager, LockerABI)
+    log("after ContractFactory")
     //const contract = (await factory.at("0x35b20fFB43c773eD6f35Cac860c7c2ddf37B8747")) as any // Rinkbey network
-    const contract = (await factory.at("0x664524fcac7f788a458709943503f79e4dd80611")) as any // Ropsten network        
+    const contract = (await factory.at("0x664524fcac7f788a458709943503f79e4dd80611")) as any // Ropsten network
+    log("after getting contract")
     const res = await contract.mintToken("bafkreid4bgisghndirgd4pna7pkojkvugmntxugar72frnkyavply4bf6u", {
       from: address,
       gas: 200000,
       value: 123
     })
+    log("after mintToken")
     log(res.toString())
   } catch (error) {
     log("executeTask error: " + error.toString())
